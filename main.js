a1d8338dd24dd11e73690bb0b2eb98cdc470c9ea
codekidhuman <96500127+codekidhuman@users.noreply.github.com>
2022-04-30 23:21:43 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+var FN = "jeremy";
+var LN = "john";
+console.log(FN)
+console.log(LN)
+FN.concat(LN)
+console.log(FN)
+var A = 24
+var B = 96
+console.log(A)
+console.log(B)
+var C = A + B
+console.log(C)
+var C = A - B
+console.log(C)
+var C = A / B
+console.log(C)
+var C = A * B
+console.log(C)
